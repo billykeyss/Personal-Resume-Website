532a41d980f5e3d62cf1f5a4046add6370123a66
Bill Huang <billhuang@live.ca>
2015-07-09 13:50:55 -0400

    Minified Js/Css/Html

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,28 @@ var htmlmin = require('gulp-htmlmin'); // minify html
 var cssmin = require('gulp-cssmin'); // minify css
 var rename = require('gulp-rename'); // rename file
 var purify = require('gulp-purifycss');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create(); // run a local version with livereload (it will automatiucally reload the page when you change something on your code)
  
 // task to minify your html
 gulp.task('html', function() {
-  return gulp.src('src/*.html') // you should change the source to fit into you project
+  return gulp.src('index.html') // you should change the source to fit into you project
     .pipe(htmlmin({collapseWhitespace: true})) // runs htmlmin
-    .pipe(gulp.dest('dist')) // where the files goes after the task is completed
+    .pipe(gulp.dest('dist/views')) // where the files goes after the task is completed
 });
  
 // taks to minify css
 gulp.task('css', function () {
-  gulp.src('src/**/*.css') // you should change the source to fit into you project
+  gulp.src('css/*.css') // you should change the source to fit into you project
     .pipe(cssmin()) // runs cssmin
     .pipe(rename({suffix: '.min'})) // add a sufix 'min' (main.css -> main.min.css)
-    .pipe(gulp.dest('dist')); // where the files goes after the task is completed
+    .pipe(gulp.dest('dist/css')); // where the files goes after the task is completed
+});
+
+gulp.task('compress', function() {
+  return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('removecss', function() {
