2a7568b4b0a957cb5619547d7dd3c657e80a0c73
Bruno Kinoshita <bukinoshita@gmail.com>
2015-07-09 15:52:25 -0300

    Architecture & Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,23 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin'); // minify html
 var cssmin = require('gulp-cssmin'); // minify css
-var rename = require('gulp-rename'); // rename file
 var purify = require('gulp-purifycss');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create(); // run a local version with livereload (it will automatiucally reload the page when you change something on your code)
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
  
 // task to minify your html
 gulp.task('html', function() {
-  return gulp.src('index.html') // you should change the source to fit into you project
+  return gulp.src('assets/views/*.html') // you should change the source to fit into you project
     .pipe(htmlmin({collapseWhitespace: true})) // runs htmlmin
     .pipe(gulp.dest('dist/views')) // where the files goes after the task is completed
 });
  
-// taks to minify css
+// task to minify css
 gulp.task('css', function () {
-  gulp.src('css/*.css') // you should change the source to fit into you project
+  gulp.src('assets/css/*.css') // you should change the source to fit into you project
     .pipe(cssmin()) // runs cssmin
-    .pipe(rename({suffix: '.min'})) // add a sufix 'min' (main.css -> main.min.css)
     .pipe(gulp.dest('dist/css')); // where the files goes after the task is completed
 });
 
@@ -33,12 +33,22 @@ gulp.task('removecss', function() {
     .pipe(purify(['./public/app/**/*.js', './public/**/*.html']))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('img', function () {
+  return gulp.src('assets/img/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/img'));
+});
  
 gulp.task('serve', ['html', 'css', 'compress'], function () {
   // Serve files from the root of this project
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./dist/"
     }
   });
 });
